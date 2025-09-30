0e9abce9458e2758977f6a8a613c9651284ab615
Simon Oulevay <simon.oulevay@gmail.com>
2025-09-30 20:32:17 +0200

    Remove old WITH mysql_native_password clause

--- todolist.sql
+++ todolist.sql
@@ -1,9 +1,8 @@
 CREATE DATABASE IF NOT EXISTS todolist;
 
--- The "WITH mysql_native_password" portion of this query is required for some
--- older versions of MySQL. If you get a "syntax error [...] near BY" message,
--- simply remove that part and use only "IDENTIFIED BY".
-CREATE USER IF NOT EXISTS 'todolist'@'localhost' IDENTIFIED WITH mysql_native_password BY 'change-me-now';
+-- Some older versions of MySQL might require you to add "WITH
+-- mysql_native_password" between "IDENTIFIED" and "BY" in the following query.
+CREATE USER IF NOT EXISTS 'todolist'@'localhost' IDENTIFIED BY 'change-me-now';
 
 GRANT ALL PRIVILEGES ON todolist.* TO 'todolist'@'localhost';
 
