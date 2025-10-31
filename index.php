42684f52f65c0a7792e3a347ff27512527758c10
Sacha Loskov <sacha.loskov@gmail.com>
2025-10-31 15:38:14 +0100

    Updated the configuration in index.php

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 // accessing the application at "http://localhost:8888/php-todo-ex/", then
 // BASE_URL should be "/php-todo-ex/". If you are accessing the application at
 // "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/php-todo-ex/');
+define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
-define('DB_USER', 'todolist');
-define('DB_PASS', 'rolusa');
-define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
+define('DB_PASS', getenv('TODOLIST_DB_PASS'));
+define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
+define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
+define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
